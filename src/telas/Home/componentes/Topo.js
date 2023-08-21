c49e47a2e81b6627b6f9f554b1c5509ec08d9b16
Matheus Lima <teulima20@hotmail.com>
2023-08-21 20:05:33 -0300

    feat: creating an app in react native

---
+++ src/telas/Home/componentes/Topo.js
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+
+
+export default function Topo() {
+    return (
+        <View>Tela de topo</View>
+    );
+}
---
+++ src/telas/Home/index.js
@@ -0,0 +1,8 @@
+// import { View } from "react-native";
+import Topo from "./componentes/Topo";
+
+export default function Home() {
+    return (
+        <Topo/>
+    );
+}
