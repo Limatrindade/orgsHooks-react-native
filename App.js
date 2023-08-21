15255439c627e391cd50ed34806eb8cce95cc4f2
Matheus Lima <teulima20@hotmail.com>
2023-08-21 20:50:12 -0300

    feat: creating mocks to make the texts dynamic

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, SafeAreaView } from 'react-native';
+
+import Home from './src/telas/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testando o react native</Text>
+    <SafeAreaView>
+      <Home/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -1,8 +1,35 @@
-import { View } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+import logoTopo from "../../../../assets/logo.png";
 
 
 export default function Topo() {
     return (
-        <View>Tela de topo</View>
+        <View style={styles.topo}>
+          <Image source={logoTopo} style={styles.imagem} />  
+          <Text style={styles.boasVindas}>Olá, Matheus Lima</Text>
+          <Text style={styles.legenda}>Encontre os melhores produtores</Text>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    topo: {
+        backgroundColor: "#F6F6F6",
+        padding: 16
+    },
+    imagem: {
+        width: 70,
+        height: 28
+    },
+    boasVindas: {
+        marginTop: 24,
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold"
+    },
+    legenda: {
+        fontSize: 16,
+        lineHeight: 26
+    }
+});
